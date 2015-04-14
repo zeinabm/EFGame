422dcb66f66328b30c45a71ec6be25f1049b9c54
zeinabm <zeinab.meftah@gmail.com>
2015-04-15 01:16:53 +0430

    ایجکس کردن پیوستن به بازی (در صفحه نمایش بازی ها)
    در حال حاضر پیغام های خطا با alert نمایش داده می شن٬ بسته به نظر مشتری
    این قسمت ممکنه تغییر کنه.

--- app/controllers/game_rosters_controller.rb
+++ app/controllers/game_rosters_controller.rb
@@ -23,30 +23,28 @@ class GameRostersController < ApplicationController
 
   def create
     @game = Game.find(params[:game_id])
-    if game_is_full?
-      redirect_to @game, notice: 'ظرفیت این بازی تکمیل است'
-    elsif @game.players.where(:id => current_user.id) and game_is_starting? 
-      flash[:notice] = 'شما قبلا به این بازی پیوسته اید و بازی شروع شده است.'
-      @game_roster = GameRoster.where(:player_id => current_user.id, :game_id => @game.id).first
-      redirect_to @game_roster
-    else
-      @game_roster = @game.game_rosters.new()
-      @game_roster.player = current_user
-      current_user.game_rosters << @game_roster
-      flash[:notice] = 'GameRoster was successfully created.' if @game_roster.save
-      #respond_with(@game_roster)
-      if game_is_starting?
-        flash[:notice] = 'بازی شروع شده است.'
-        redirect_to @game_roster
+    @game_roster = @game.game_rosters.new()
+    @game_roster.player = current_user
+    current_user.game_rosters << @game_roster
+    
+    validate_action_create
+    @game_started = true if game_is_starting?
+    
+    respond_to do |format|
+      if @game_roster.save
+        format.html { redirect_to @game_roster, notice: 'game_roster was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @game_roster }
+        format.js
       else
-        redirect_to @game
+        format.html { render action: 'new' }
+        format.json { render json: @game_roster.errors, status: :unprocessable_entity }
+        format.js
       end
     end
   end
 
   def update
-    score = calc_score
-    flash[:notice] = 'امتیازی که از مرحله قبل به دست آوردید برابر ' + score.to_s + ' است.' if @game_roster.update(game_roster_params)
+    flash[:notice] = 'GameRoster was successfully updated.' if @game_roster.update(game_roster_params)
     respond_with(@game_roster)
   end
 
@@ -56,15 +54,21 @@ class GameRostersController < ApplicationController
   end
   
   private
-    def calc_score
-      score = 0;
-      return score;
-    end
     def game_is_starting?
       return @game.players.count >= @game.number_of_players
     end
     def game_is_full?
       return @game.players.count > @game.number_of_players
+    end
+    
+    def validate_action_create
+      @errors = []
+      if game_is_full?
+        @errors << 'ظرفیت این بازی تکمیل است'
+      end
+      if @game.players.where(:id => current_user.id).count != 0 and game_is_starting?
+        @errors << 'شما قبلا به این بازی پیوسته اید و بازی شروع شده است.'
+      end
     end
     
     def set_game_roster
