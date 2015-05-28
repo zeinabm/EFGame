fc5dc452ecfe35155ca529e9759f3787da631ccb
fghanadi <fateme.ghanadi@gmail.com>
2015-05-28 10:49:49 +0430

    news_controller

---
+++ app/controllers/news_controller.rb
@@ -0,0 +1,123 @@
+# class NewsController < ApplicationController
+#   before_action :set_news, only: [:show, :edit, :update, :destroy]
+
+#   respond_to :html
+
+#   def index
+#     @news = News.all
+#     respond_with(@news)
+#   end
+
+#   def show
+#     respond_with(@news)
+#   end
+
+#   def new
+#     @news = News.new
+#     respond_with(@news)
+#   end
+
+#   def edit
+#   end
+
+#   def create
+#     @news = News.new(news_params)
+#     flash[:notice] = 'News was successfully created.' if @news.save
+#     respond_with(@news)
+#   end
+
+#   def update
+#     flash[:notice] = 'News was successfully updated.' if @news.update(news_params)
+#     respond_with(@news)
+#   end
+
+#   def destroy
+#     @news.destroy
+#     respond_with(@news)
+#   end
+
+#   private
+#     def set_news
+#       @news = News.find(params[:id])
+#     end
+
+#     def news_params
+#       params.require(:news).permit(:title, :content)
+#     end
+# end
+
+class NewsController < ApplicationController
+  before_action :set_news, only: [:show, :edit, :update, :destroy]
+
+  # GET /news
+  # GET /news.json
+  def index
+    @news = News.all
+  end
+
+  # GET /news/1
+  # GET /news/1.json
+  def show
+  end
+
+  # GET /news/new
+  def new
+    @news = News.new
+  end
+
+  # GET /news/1/edit
+  def edit
+  end
+
+  # POST /news
+  # POST /news.json
+  def create
+    @news = News.new(news_params)
+
+    respond_to do |format|
+      if @news.save
+        format.html { redirect_to controller: 'profiles', action:'show'  }
+        format.json { render 'profiles/show', status: :created, location: @news }
+        format.js {}
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @news.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /news/1
+  # PATCH/PUT /news/1.json
+  def update
+    respond_to do |format|
+      if @news.update(news_params)
+        format.html  { redirect_to controller: 'profiles', action:'show'  }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @news.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /news/1
+  # DELETE /news/1.json
+  def destroy
+    @news.destroy
+    respond_to do |format|
+      format.html { redirect_to controller: 'profiles', action:'show'  }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_news
+      @news = News.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def news_params
+      params.require(:news).permit(:title, :content)
+    end
+end
